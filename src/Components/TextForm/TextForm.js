43e89f4bb7560ec53e4ad2101e42661bc7e2fb09
wasifwaheed17 <wasifwaheed17@gmail.com>
2023-08-21 13:45:46 +0500

    major changes

--- src/Components/TextForm/TextForm.js
+++ src/Components/TextForm/TextForm.js
@@ -30,16 +30,30 @@ export default function TextForm(props) {
 
     const [text, setText] = useState("enter here");
     return(
-    <div>
-        
+    <>
+        <div>
 <div className="mb-3">
     <h1>{props.heading}</h1>
   <textarea className="form-control" value={text} onChange={handelonchange} id="textBox" rows="8"></textarea>
 </div>
-  <button className="btn btn-primary" onClick={handleupclick} >Convert To Uppercase</button>
-  <button className="btn btn-primary" onClick={handleloclick} >Convert To Lowercase</button>
-  <button className="btn btn-danger" onClick={handledoclick} >Remove Text</button>
-    </div>
+< div className="container">
+  <button className="btn btn-primary mx-2" onClick={handleupclick} >Convert To Uppercase</button>
+  
+  <button className="btn btn-primary mx-2" onClick={handleloclick} >Convert To Lowercase</button>
+  <button className="btn btn-danger mx-2" onClick={handledoclick} >Remove Text</button>
+  </div>
+  <div className="container my-3">
+    <h5>Your Text summary</h5>
+    <p>{text.split(" ").length} words and {text.length}charcters</p>
+    <p>{0.008*text.split(" ").length}minutes to read</p>
+    <h5>Preview</h5>
+    <p>{text}</p>
+  </div>
+
+
+  
+  </div>
+  </>
     )
 }
 
